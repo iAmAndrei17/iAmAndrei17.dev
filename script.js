afbb496b42584475c854fb0ced4b3e83da6f058d
iAmAndrei17 <achimescuandrei17@yahoo.com>
2023-10-21 12:07:20 +0300

    iAmAndrei17.dev Portofolio Website

---
+++ script.js
@@ -0,0 +1,22 @@
+//INIT AOS ANIMATION LIBRARY
+AOS.init();
+
+//ANIMATION FUNCTION (5s)
+function startAnimation(animationName, elementId) {
+  const element = document.getElementById(elementId);
+  if(element) {
+    element.classList.add('animate__animated', animationName, 'animate__infinite');
+
+    setTimeout(() => {
+      element.classList.remove(animationName);
+    }, 1000);
+  }
+}
+
+setInterval(function() {
+  startAnimation('animate__pulse', 'web-developer-title');
+}, 5000);
+
+setInterval(function() {
+  startAnimation('animate__flash', 'andrei-dev-github');
+}, 3000);
